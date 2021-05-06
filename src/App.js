c862e3d243a9484860253da7ad4d415a72671dd7
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-05 23:08:25 -0500

    splited up a component according their responsability
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -28,22 +28,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpensiveItem>
+      />
       <ExpensiveItem 
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpensiveItem>
+      />
       <ExpensiveItem 
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpensiveItem>
+      />
       <ExpensiveItem 
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpensiveItem>
+      />
     </div>
   );
 }
