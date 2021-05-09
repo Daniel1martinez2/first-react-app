ee10aa2026604004441c0d83dde2d4583bd59dc0
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-08 23:53:19 -0500

    state hook, display new data info dynamically
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, {useState} from 'react';
 import Card from '../UI/Card'; 
 import ExpenseDate from './ExpenseDate'; 
 import './ExpenseItem.css'
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); 
+  const [amount, setAmount] = useState(props.amount); 
+  const clickHandler = ()=>{
+    setTitle('updated'); 
+    setAmount(0); 
+    console.log(title);
+  }; 
   return (
   <Card className="expense-item">
     <ExpenseDate date={props.date}/> {/*self closing and this is a comment*/}
     <div className="expense-item__description" >
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">$ {props.amount}</div>
+      <h2>{title}</h2>
+      <div className="expense-item__price">$ {amount}</div>
     </div>
+    <button onClick ={clickHandler} >Change title</button>
   </Card>
   ); 
 }
-
 export default ExpenseItem; 
