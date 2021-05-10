cf127d5bde39290ad59e5b446f889b0a45c8eb85
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-09 23:22:24 -0500

    understanding ways of updating data
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,29 +1,38 @@
 import React, {useState} from 'react'; 
 import './ExpenseForm.css'; 
 const ExpenseForm = () => { 
-  const [userInput,setUserInput]= useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: '',
-  }); 
-
+  //alternative 1
+  const [enteredTitle, setEnteredTitle]=useState(''); 
+  const [enteredAmount, setEnteredAmount]=useState(''); 
+  const [enteredDate, setEnteredDate]=useState('');
+  //alternative 2
+  // const [userInput,setUserInput]= useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // }); 
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });  
+    setEnteredTitle(event.target.value ); 
+    // setUserInput((prevState)=>{
+    //   return {
+    //     ...prevState,
+    //     enteredTitle:event.target.value, 
+    //   }
+    // });  
   };
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value ); 
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value ); 
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
   return (
     <form>
